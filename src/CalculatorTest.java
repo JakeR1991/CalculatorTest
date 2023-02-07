48c4863dc0c4fdd133974f83130b513771761887
JakeR1991 <jakerogoff@hotmail.com>
2023-02-07 12:06:45 +0000

    Commit

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -4,4 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
+    void twoPlusEightEqualsTen() {
+        // create object to test
+        var cal = new Calculator();
+        assertEquals(10, cal.add (2, 8));
+    }
 }
