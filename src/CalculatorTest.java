b2d4de45a2f960373549913c24b40ba3f8d8492b
JakeR1991 <jakerogoff@hotmail.com>
2023-02-07 12:15:58 +0000

    Commit

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -15,4 +15,16 @@ class CalculatorTest {
         var cal = new Calculator();
         assertEquals(8, cal.add (2, 6));
     }
+    @Test
+    void fivePlusFiveEqualsTen() {
+        // create object to test
+        var cal = new Calculator();
+        assertEquals(10, cal.add (5, 6));
+    }
+    @Test
+    void twoTimesSixEqualsTwelve() {
+        // create object to test
+        var cal = new Calculator();
+        assertEquals(12, cal.multiply (2, 6));
+    }
 }
